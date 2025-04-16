6014f469261ede2d15f5a2997d791a5fbbb38083
Kiljune Choi <kiljune@gmail.com>
2025-04-16 16:26:41 +0900

    add ibus

---
+++ modules/system/ibus.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  config,
+  options,
+  lib,
+  inputs,
+  system,
+  systemSettings,
+  userSettings,
+  ...
+}: {
+  i18n = {
+    inputMethod = {
+      type = "ibus";
+      enable = true;
+      ibus.engines = with pkgs.ibus-engines; [ hangul ];
+    };
+  };
+}
