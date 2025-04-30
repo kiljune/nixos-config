dd8637fbe2f5cfae8ea8d446f77b2322bd06ed3b
Kiljune Choi <kiljune@gmail.com>
2025-04-30 16:35:51 +0900

    add firewalld-gui

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -19,4 +19,8 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    firewalld-gui
+  ];
 }
