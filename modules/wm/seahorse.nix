a3902e6951e09474db27d02ac015823a83f0d977
Kiljune Choi <kiljune@gmail.com>
2025-06-10 08:48:55 +0900

    add seahorse

---
+++ modules/wm/seahorse.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  config,
+  options,
+  lib,
+  inputs,
+  system,
+  systemSettings,
+  userSettings,
+  ...
+}: {
+  programs.seahorse.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.service = {
+    greetd.enableGnomeKeyring = true;
+    greetd-password.enableGnomeKeyring = true;
+    login.enableGnomeKeyring = true;
+  };
+
+  services.dbus.packages = [pkgs.gnome-keyring pkgs.gcr];
+}
